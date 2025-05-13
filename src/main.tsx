74985dee2ed6e4c6013fb0aefb28dab5c8317a67
electroglodyte <131520505+electroglodyte@users.noreply.github.com>
2025-05-13 17:29:30 +0100

    Update main.tsx to use Routes component

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './App.css'
-
-// Ensure root element is properly styled
-if (document.getElementById('root')) {
-  document.getElementById('root')!.style.cssText = `
-    height: 100%;
-    width: 100%;
-    display: block;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-  `;
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Routes from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
-)
+);
