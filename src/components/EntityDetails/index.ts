40ab1967f5fe319192c000eb0660a6688c1e149a
electroglodyte <131520505+electroglodyte@users.noreply.github.com>
2025-05-16 23:15:13 +0100

    Add index file for EntityDetails components

---
+++ src/components/EntityDetails/index.ts
@@ -0,0 +1,4 @@
+export { CharacterDetails } from './CharacterDetails';
+export { LocationDetails } from './LocationDetails';
+export { FactionDetails } from './FactionDetails';
+export { ItemDetails } from './ItemDetails';
