1c47927e3e03f6ff34257eb9dff7d7dc57c80a47
electroglodyte <131520505+electroglodyte@users.noreply.github.com>
2025-05-17 00:15:51 +0100

    Add theme provider component

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useLocalStorage } from '@/hooks/use-local-storage'
+
+type Theme = 'dark' | 'light' | 'system'
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  defaultTheme?: Theme
+  storageKey?: string
+}
+
+export function ThemeProvider({
+  children,
+  defaultTheme = 'system',
+  storageKey = 'vite-ui-theme',
+  ...props
+}: ThemeProviderProps) {
+  const [theme, setTheme] = useLocalStorage<Theme>(
+    storageKey,
+    defaultTheme
+  )
+
+  React.useEffect(() => {
+    const root = window.document.documentElement
+
+    root.classList.remove('light', 'dark')
+
+    if (theme === 'system') {
+      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
+        .matches
+        ? 'dark'
+        : 'light'
+
+      root.classList.add(systemTheme)
+      return
+    }
+
+    root.classList.add(theme)
+  }, [theme])
+
+  return (
+    <div {...props}>
+      {children}
+    </div>
+  )
+}
