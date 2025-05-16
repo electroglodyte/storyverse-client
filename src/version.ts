5de4191dc86a769f36d877ffbb1443950cee16a3
electroglodyte <131520505+electroglodyte@users.noreply.github.com>
2025-05-16 08:26:06 +0100

    Update version number for story_world_id fix

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 export const version = {
   major: 0,
   minor: 9,
-  patch: 65,
+  patch: 66,
   date: '2025-05-16',
-  notes: 'Fix story_id reference in Story Importer by using proper UUID'
+  notes: 'Fix story_world_id foreign key constraint in Story Importer'
 };
