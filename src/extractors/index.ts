8fba1f1782dbd2dbb5a4c6335d76fca10cb3261b
electroglodyte <131520505+electroglodyte@users.noreply.github.com>
2025-05-16 13:45:08 +0100

    Create extractors index file

---
+++ src/extractors/index.ts
@@ -0,0 +1,7 @@
+// Export all extractors from this file
+export { extractCharacters } from './characterExtractor';
+export { extractLocations } from './locationExtractor';
+export { extractPlotlines } from './plotlineExtractor';
+export { extractEvents } from './eventExtractor';
+export { extractObjects } from './objectExtractor';
+export { extractScenes } from './sceneExtractor';
