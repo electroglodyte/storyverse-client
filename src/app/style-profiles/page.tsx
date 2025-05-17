b527dd2c05410c67c2c8bb4e3174ef0c95b05bc1
electroglodyte <131520505+electroglodyte@users.noreply.github.com>
2025-05-17 05:30:48 +0100

    Add style profiles list page

---
+++ src/app/style-profiles/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleProfileList } from '@/components/StyleProfileList';
+
+export default function StyleProfilesPage() {
+  return (
+    <div className="container mx-auto py-8">
+      <StyleProfileList />
+    </div>
+  );
+}
