b890d459031eab17dad40ea2f5b0b7d2168e9bf7
electroglodyte <131520505+electroglodyte@users.noreply.github.com>
2025-05-17 09:30:59 +0100

    Add index file for entity components

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+// Entity detail components
+export { default as CharacterDetails } from './entities/CharacterDetails';
+export { default as LocationDetails } from './entities/LocationDetails';
+export { default as FactionDetails } from './entities/FactionDetails';
+
+// Form components
+export { default as EntityForm } from './forms/EntityForm';
+export { default as CharacterForm } from './forms/CharacterForm';
+export { default as LocationForm } from './forms/LocationForm';
+export { default as FactionForm } from './forms/FactionForm';
+
+// Types
+export type { FormFieldDefinition } from './forms/EntityForm';
